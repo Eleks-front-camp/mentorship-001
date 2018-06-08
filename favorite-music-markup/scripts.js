3fa6b9616f8a7d198d13f0759c3701dad89a4eae
Serhiy.Koziuk <serhiy.koziuk@eleks.com>
2018-06-08 17:05:54 +0300

    added progressBar

--- favorite-music-markup/scripts.js
+++ favorite-music-markup/scripts.js
@@ -26,11 +26,16 @@ var soundsAPI = new Promise(function (resolve, reject) {
   reject(new Error('Some error message....'));
 });
 
-function SoundsList() {
-  this.audioPlayer = document.querySelector('#audioPlayer');
-  this.controlBtn = document.querySelector('.fm-control-btn');
-  this.currentAudio = null;
-  this.isPlay = false;
+var helper = {
+  getObjectById: function (data, id) {
+    return data.filter(function (item) {
+      return item.id == id }
+    )[0];
+  }
+};
+
+function SoundsList(_player) {
+  this.player = _player;
 
   this._getSoundsList();
 }
@@ -38,7 +43,8 @@ function SoundsList() {
 SoundsList.prototype._getSoundsList = function () {
   soundsAPI.then(
     function (data) {
-      this._createList(data)
+      this._createList(data);
+      soundsData = data;
     }.bind(this),
     function (err) { err.message }
   );
@@ -50,77 +56,124 @@ SoundsList.prototype._createList = function (list) {
       .insertAdjacentHTML('beforeend', this._createTemplate(item));
   }.bind(this));
 
-  this._addEventInSoundCard();
+  this.player._addEventInSoundCard();
 }
 
 SoundsList.prototype._createTemplate = function (item) {
-  return `
-      <div class="fm-sound-card"
-        data-sound="${item.sound}"
-        style="
-          background-image: url('${item.img}');
-        ">
-        <div class="fm-sound-card-content">
-          <h4 class="fm-sound-card-title">${item.author}</h4>
-          <p class="fm-sound-card-description">${item.name}</p>
-        </div>
-      </div>
-    `;
+  return '<div class="fm-sound-card"'+
+    'data-id="' + item.id + '"' +
+    'style="background-image: url('+ item.img +');">'+
+        '<div class="fm-sound-card-content">'+
+          '<h4 class="fm-sound-card-title">' + item.author + '</h4>'+
+          '<p class="fm-sound-card-description">' + item.name + '</p>'+
+        '</div>'+
+      '</div>';
 }
 
-SoundsList.prototype._addEventInSoundCard = function () {
+function Player() {
+  this.audioPlayer = document.querySelector('#audioPlayer');
+  this.controlBtn = document.querySelector('.fm-control-btn');
+  this.songBand = document.querySelector('.fm-song-band');
+  this.songName = document.querySelector('.fm-song-name');
+  this.progressBar = document.querySelector('#fm-progress-bar');
+  this.currentAudio = null;
+  this.isPlay = false;
+
+  this._audioPlayerEvents();
+
+  this.progressBar.addEventListener('click', function (e) {
+    if (this.audioPlayer.src) {
+      var percent = e.offsetX / this.progressBar.offsetWidth;
+
+      this.audioPlayer.currentTime = percent * this.audioPlayer.duration;
+      e.target.value = Math.floor(percent / 100);
+      e.target.innerHTML = this.progressBar.value + '% played';
+    }
+  }.bind(this));
+}
+
+Player.prototype._audioPlayerEvents = function () {
+  this.audioPlayer.addEventListener('ended', function () {
+    this.pause();
+  });
+
+  this.audioPlayer.addEventListener('timeupdate', function () {
+    this._updateProgressBar();
+  }.bind(this));
+}
+
+Player.prototype._updateProgressBar = function () {
+  var percentage = 0;
+
+  if (this.audioPlayer.duration) {
+    percentage = (100 / this.audioPlayer.duration) * this.audioPlayer.currentTime;
+  }
+
+  this.progressBar.value = percentage;
+};
+
+Player.prototype._addEventInSoundCard = function () {
   document.querySelectorAll('.fm-sound-card').forEach(
     function (item) {
       item.addEventListener('click', function (e) {
-        this._playSounds(e)
+        this._playSounds(e);
       }.bind(this));
     }.bind(this)
   );
-}
+};
 
-SoundsList.prototype._playSounds = function (e) {
-  var el = e.target;
+Player.prototype._playSounds = function (e) {
+  var el = e.target,
+    currentAudioData;
 
   while (true) {
-    if (el.dataset.sound || el == document.body) break;
+    if (el.dataset.id || el == document.body) break;
     el = el.parentElement;
   }
 
-  if (this.currentAudio == el) {
+  currentAudioData = helper.getObjectById(soundsData, el.dataset.id);
+
+  if (this.currentAudio == currentAudioData) {
     if (this.isPlay) {
       this.isPlay = false;
       this.audioPlayer.pause();
-      controlBtnState.call(this, 'pause');
+      this._controlBtnState.call(this, 'pause');
     } else {
       this.isPlay = true;
       this.audioPlayer.play();
-      controlBtnState.call(this, 'play');
+      this._controlBtnState.call(this, 'play');
     }
   } else {
     this.isPlay = true;
-    this.currentAudio = el;
-    this.audioPlayer.src = el.dataset.sound;
+    this.currentAudio = currentAudioData;
+    this.audioPlayer.src = currentAudioData.sound;
     this.audioPlayer.play();
-    controlBtnState.call(this, 'play');
+    this._controlBtnState.call(this, 'play');
+    this.songBand.textContent = currentAudioData.author;
+    this.songName.textContent = currentAudioData.name;
   }
-
-  function controlBtnState(state) {
-    switch (state.trim().toLowerCase()) {
-      case 'play':
-        this.controlBtn.classList.remove('fm-control-btn-play');
-        this.controlBtn.classList.add('fm-control-btn-pause');
-        break;
-      case 'pause':
-        this.controlBtn.classList.remove('fm-control-btn-pause');
-        this.controlBtn.classList.add('fm-control-btn-play');
-        break;
-      default:
-        break;
-    }
-  };
 
 }
 
+Player.prototype._controlBtnState = function (state) {
+  switch (state.trim().toLowerCase()) {
+    case 'play':
+      this.controlBtn.classList.remove('fm-control-btn-play');
+      this.controlBtn.classList.add('fm-control-btn-pause');
+      break;
+    case 'pause':
+      this.controlBtn.classList.remove('fm-control-btn-pause');
+      this.controlBtn.classList.add('fm-control-btn-play');
+      break;
+    default:
+      break;
+  }
+};
+
 window.onload = function () {
-  var soundsList = new SoundsList();
+  var soundsData = [];
+
+  var player = new Player();
+
+  var soundsList = new SoundsList(player);
 };
